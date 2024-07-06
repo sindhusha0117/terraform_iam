5cf040ba4f1fb7e1b57b63672797a4b9080ea654
Bala Sindhusha Boyapati <gbala0117@gmail.com>
2024-07-05 23:56:20 -0400

    Infra setup

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ module "data_policy_document" {
   source = "./modules/data"
 }
 
-module "create_s3_policy" {
-  source      = "./modules/create_policy"
-  policy_name = "s3_full_access_mgd"
-  policy_doc  = module.data_policy_document.s3_full_access_doc
-}
+# module "create_s3_policy" {
+#   source      = "./modules/create_policy"
+#   policy_name = "s3_full_access_mgd"
+#   policy_doc  = module.data_policy_document.s3_full_access_doc
+# }
 
 module "create_dev_group" {
   source = "./modules/create_groups"
@@ -32,7 +32,7 @@ module "attach_policy_to_dev_group" {
   source          = "./modules/attach_policies_groups"
   attachment_name = "dev_group_attachment"
   group_name      = module.create_dev_group.created_group_name
-  policies_to_attach = { "s3_policy" = module.create_s3_policy.policy_arn,
+  policies_to_attach = { "s3_policy" = "arn:aws:iam::aws:policy/AmazonS3FullAccess",
     "gl_policy" = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess",
   "cw_policy" = "arn:aws:iam::aws:policy/CloudWatchEventsFullAccess" }
 }
@@ -48,7 +48,7 @@ module "attach_policies_to_glue_role" {
   source          = "./modules/attach_policies_roles"
   attachment_name = "glue_role_policy_attachment"
   role_name       = module.create_glue_role.role_name
-  policies_to_attach = { "s3_policy" = module.create_s3_policy.policy_arn,
+  policies_to_attach = { "s3_policy" = "arn:aws:iam::aws:policy/AmazonS3FullAccess",
     "gl_policy" = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole",
   "cw_policy" = "arn:aws:iam::aws:policy/CloudWatchFullAccess" }
 }
